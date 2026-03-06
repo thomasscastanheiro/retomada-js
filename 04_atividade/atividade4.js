542c7a74769d86286cad09d5870ecb3b9b88f8f5
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2026-03-06 16:33:44 -0300

    finalizado

--- 04_atividade/atividade4.js
+++ 04_atividade/atividade4.js
@@ -1,15 +1,17 @@
 // adiciona as bolinhas
-    const btnBolinhas = document.getElementById("addBolinhas");
-    let total = 0; // contador de bolinhas
-    let bolinhas = ""; // conteudo das bolinhas
-    let bola = '<div class="bolinha></div>'; // conteudo para adicionar
+const btnBolinhas = document.getElementById("addBolinhas");
+let total = 0; // contador de bolinhas
+let bolinhas = ""; // conteúdo das bolinhas
+let bola = '<div class="bolinha"></div>'; // conteúdo para adicionar
 
 // ação de clique
-    btnBolinhas.addEventListener("click", () => {
-        total = total+1;
-        bolinhas = "";
-        for (let index = 0; index < total; index++) {
-            bolinhas = bolinhas+bola;
-        }
-        document.getElementById("agrupaBolinhas").innerHTML = bolinhas;
-    });
+btnBolinhas.addEventListener("click", () => {
+    total = total + 1;
+    bolinhas = "";
+
+    for (let i = 0; i < total; i++) {
+        bolinhas = bolinhas + bola;
+    }
+
+    document.getElementById("agrupaBolinhas").innerHTML = bolinhas;
+});
