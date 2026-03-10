5f0ac5aa60f385850e4bd561a064d1db18f55ef3
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2026-03-10 08:14:41 -0300

    removr bolinhas

--- 04_atividade/atividade4.js
+++ 04_atividade/atividade4.js
@@ -1,5 +1,8 @@
 // adiciona as bolinhas
 const btnBolinhas = document.getElementById("addBolinhas");
+const btnRemoverBolinhas = document.getElementById("removerBolinhas");
+const btnBolinhas5 = document.getElementById("addBolinhas5");
+const btnRemoverBolinhas5 = document.getElementById("removerBolinhas5");
 let total = 0; // contador de bolinhas
 let bolinhas = ""; // conteúdo das bolinhas
 let bola = '<div class="bolinha"></div>'; // conteúdo para adicionar
@@ -14,4 +17,37 @@ btnBolinhas.addEventListener("click", () => {
     }
 
     document.getElementById("agrupaBolinhas").innerHTML = bolinhas;
+});
+
+btnRemoverBolinhas.addEventListener("click", () => {
+    total = total - 1;
+    bolinhas = "";
+
+    for (let i = 0; i < total; i++) {
+        bolinhas = bolinhas + bola;
+    }
+
+    document.getElementById("agrupaBolinhas").innerHTML  = bolinhas;
+});
+
+btnRemoverBolinhas5.addEventListener("click", () => {
+    total = total - 5;
+    bolinhas = "";
+
+    for (let i = 0; i < total; i++) {
+        bolinhas = bolinhas + bola;
+    }
+
+    document.getElementById("agrupaBolinhas").innerHTML  = bolinhas;
+});
+
+btnBolinhas5.addEventListener("click", () => {
+    total = total + 5;
+    bolinhas = "";
+
+    for (let i = 0; i < total; i++) {
+        bolinhas = bolinhas + bola;
+    }
+
+    document.getElementById("agrupaBolinhas").innerHTML  = bolinhas;
 });
